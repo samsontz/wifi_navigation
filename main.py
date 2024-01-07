d8863a96f259ca9444a1f00ec44529256d7e631d
samsontz <franksam018@gmail.com>
2024-01-07 14:45:59 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel
 import pickle
 import json
 
-
 app = FastAPI()
 
 origins = ["*"]
@@ -17,32 +16,29 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-class model_input(BaseModel):
-    
+class ModelInput(BaseModel):
     NMAIST_D: int
     NMAIST_B: int
     NMAIST_C: int
     CDAC_14: int
-       
 
 # loading the saved model
-wifi_model = pickle.load(open('random_forest_model.pkl','rb'))
-
+wifi_model = pickle.load(open('random_forest_model.pkl', 'rb'))
 
 @app.post('/predict')
-def wifi_pred(input_parameters : model_input):
-    
+def wifi_pred(input_parameters: ModelInput):
     input_data = input_parameters.json()
     input_dictionary = json.loads(input_data)
-    
+
     a = input_dictionary['NMAIST_D']
     b = input_dictionary['NMAIST_B']
     c = input_dictionary['NMAIST_C']
     dac = input_dictionary['CDAC_14']
 
-
     input_list = [a, b, c, dac]
-    
     prediction = wifi_model.predict([input_list])
-    
 
+    # Extracting X and Y predictions
+    output_x, output_y = prediction[0]
+
+    return {"output_x": output_x, "output_y": output_y}
